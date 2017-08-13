6b3212f8b668367850292eee834062a30e54e948
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-08-13 14:18:20 +0300

    create board container. create board.json file with a test board object

--- bird-is-the-word-client/src/containers/App.js
+++ bird-is-the-word-client/src/containers/App.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react'
 import './App.css'
+import Board from './Board'
+import board from '../board.json'
 
 class App extends Component{
   render(){
+    console.log(board)
     return (
       <div className="App">
         <h1>Bird Is the Word</h1>
+        
       </div>
     )
   }
