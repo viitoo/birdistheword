906bd8cb2096d4168efa7dbe6eb3a8e2a09f8453
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-08-13 22:35:36 +0300

    add tile component for rack

---
+++ bird-is-the-word-client/src/components/Tile.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Tile = (props) => (
+    <th className="square">{props.letter}</th>
+)
+
+export default Tile;
--- bird-is-the-word-client/src/containers/Rack.js
+++ bird-is-the-word-client/src/containers/Rack.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import Tile from '../components/Tile'
 
 const Rack = (props) => {
   
   const rack = props.players.players[0].rack.map(letter => {
-    return <th className="square"> {letter} </th>
-  })
+    return <Tile letter={letter} />
+    }
+  )
   return(
     <div>
       <h1>{props.players.players[0].name}</h1>
