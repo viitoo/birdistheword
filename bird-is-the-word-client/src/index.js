97227132a2de6bc8c7b1a589d827e1afc635cc1a
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-08-29 11:36:42 -0700

    use createStore function to define store, create initial board reducer, import store to index.js

--- bird-is-the-word-client/src/index.js
+++ bird-is-the-word-client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store.js'
 
 ReactDOM.render(
   <App />, 
--- bird-is-the-word-client/src/store.js
+++ bird-is-the-word-client/src/store.js
@@ -3,3 +3,25 @@ import {
   applyMiddleware,
   combineReducers
 } from 'redux'
+
+import thunk from 'redux-thunk'
+
+const boardReducer = (state = [], action) => {
+  switch(action.type){
+    case 'GET_BOARD':
+      return action.board
+    default:
+      return state;
+  }
+}
+
+const reducers = combineReducers({
+  board: boardReducer
+})
+const middleware = [thunk]
+
+export default createStore(
+  reducers,
+  applyMiddleware(...middleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
