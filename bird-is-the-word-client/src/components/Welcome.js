e9c4f9d4be5aff9139a15f99ad94074f33123f05
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-09-05 15:35:23 -0700

    test adding a link to the new game on the welcome page

--- bird-is-the-word-client/src/components/Welcome.js
+++ bird-is-the-word-client/src/components/Welcome.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Welcome extends Component{
   render(){
     return(
       <div>
-        Welcome to Bird Is the Word!
+        <h1>Welcome to Bird Is the Word!</h1>
+        <Link to={"/game"}>New Game</Link>
+
       </div>
     )
   }
