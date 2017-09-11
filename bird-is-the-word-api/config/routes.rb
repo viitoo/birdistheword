e76caa88bda399e14dd52d8040f5ef5bc3ce5f58
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-09-11 12:07:41 -0700

    add auth/refresh route

--- bird-is-the-word-api/config/routes.rb
+++ bird-is-the-word-api/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :users, except: [:new, :edit]
     post '/auth', to: "auth#login"
+    post 'auth/refresh', to: "auth#refresh"
     resources :games, except: [:new, :edit]
   end
 end
