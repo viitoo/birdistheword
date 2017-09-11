7642556b23369d1a0825ef374d260f5b1d494cd3
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-09-11 13:27:56 -0700

    add :authenticate_token! helper method inside application controller. add refresh action to auth controller. add decode_token method to Auth library.

--- bird-is-the-word-api/app/controllers/api/auth_controller.rb
+++ bird-is-the-word-api/app/controllers/api/auth_controller.rb
@@ -1,5 +1,5 @@
 class Api::AuthController < ApplicationController
-
+  before_action :authenticate_token!, only: [:refresh]
   def login
     @user = User.find_by(username: params[:user][:username])
     if !@user
@@ -17,7 +17,9 @@ class Api::AuthController < ApplicationController
         }
       }, status: 500
     end
-
   end
 
+  def refresh
+    render 'users/user_with_token.json.jbuilder', user: @user
+  end
 end
