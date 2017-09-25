5588b8e5e2ad5eccb06e62b809d7aa68d65e5943
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-09-24 17:54:08 -0700

    create new GamePlayer if Player 2 is joining an existing game

--- bird-is-the-word-api/app/controllers/api/games_controller.rb
+++ bird-is-the-word-api/app/controllers/api/games_controller.rb
@@ -34,6 +34,21 @@ class Api::GamesController < ApplicationController
   end
 
   def show
+    #create new GamePlayer if Player 2 is joining this game
+    if !GamePlayer.find_by(user_id: @user.id, game_id: @game.id)
+      rack = @game.bag.sample(7)
+      rack.each{|tile_id| @game.bag.delete tile_id }
+
+      counter = 100
+
+      rack.each do |tile_id| 
+        tile = @game.tiles.select{|tile| tile["id"] == tile_id.to_i }
+        tile[0]["x"] = 1
+        tile[0]["y"] = counter
+        counter += 1
+      end
+      GamePlayer.create(user_id: @user.id, game_id: @game.id, rack: rack)
+    end
     current_user_rack = GamePlayer.find_by(user_id: @user.id, game_id: @game.id).rack
     players = @game.players
     render json: @game.attributes.merge({current_user_rack: current_user_rack, players: players})
@@ -41,6 +56,7 @@ class Api::GamesController < ApplicationController
 
   def update
     if @game.update(game_params)
+      #renew user's rack
       render json: @game
     else
       render json: {message: "Error. Try again."}
