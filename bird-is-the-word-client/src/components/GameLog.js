86fa5ffdc1b68b7596e99874f2a4b849a38e4aad
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-10-19 18:57:05 -0700

    change conditions for user taking turns to fix the bug

--- bird-is-the-word-client/src/components/GameLog.js
+++ bird-is-the-word-client/src/components/GameLog.js
@@ -5,13 +5,20 @@ const GameLog = (props) => {
   var gameLog = ""
   if (props.player_1 !== null && props.player_2 !== null && props.player_2.turns.length > 0){
 
-  const gameTurns = props.player_1.turns.map(function(turn, index) { return [turn, props.player_2.turns[index]]}).reduce(function(a,b) { return a.concat(b); })
-   
+  const gameTurns = props.player_1.turns.map(function(turn, index) { 
+    if (props.player_2.turns[index]){
+      return [turn, props.player_2.turns[index]]
+    }else{
+      return[turn]
+    }
+    
+    }).reduce(function(a,b) { return a.concat(b); })
+   console.log(gameTurns)
    gameLog = gameTurns.map((turn, index) => {
-      if (index % 2 !== 0 && props.player_2.turns.length > 0){
-        return(<p>{props.player_2.username + " played " + turn.played_word + " for " + turn.points + " points"}</p>)
-      } else {
+      if (index % 2 === 0 && props.player_2.turns.length > 0){
         return(<p>{props.player_1.username + " played " + turn.played_word + " for " + turn.points + " points"}</p>)
+      } else {
+        return(<p>{props.player_2.username + " played " + turn.played_word + " for " + turn.points + " points"}</p>)
        }
       })
 
