e87bb9b0b878cc2a2cd457297853a0fabea17ee2
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-10-20 12:37:15 -0700

    handle displaying skipped turn in game log

--- bird-is-the-word-client/src/components/GameLog.js
+++ bird-is-the-word-client/src/components/GameLog.js
@@ -16,14 +16,28 @@ const GameLog = (props) => {
    console.log(gameTurns)
    gameLog = gameTurns.map((turn, index) => {
       if (index % 2 === 0 && props.player_2.turns.length > 0){
-        return(<p>{props.player_1.username + " played " + turn.played_word + " for " + turn.points + " points"}</p>)
+        if (turn.played_word === "skipped turn"){
+          return(<p>{props.player_1.username  + " " + turn.played_word}</p>)
+        }else{
+          return(<p>{props.player_1.username + " played " + turn.played_word + " for " + turn.points + " points"}</p>)
+        }
+        
       } else {
-        return(<p>{props.player_2.username + " played " + turn.played_word + " for " + turn.points + " points"}</p>)
+        if (turn.played_word === "skipped turn"){
+          return(<p>{props.player_2.username + " " + turn.played_word}</p>)
+        }else{
+           return(<p>{props.player_2.username + " played " + turn.played_word + " for " + turn.points + " points"}</p>)
+        }    
        }
       })
 
   } else if (props.player_1 !== null && props.player_1.turns.length > 0){
-    gameLog = <p>{props.player_1.username + " played " + props.player_1.turns[0].played_word + " for " + props.player_1.turns[0].points + " points"}</p>
+    if (props.player_1.turns[0].played_word === "skipped turn"){
+      gameLog = <p>{props.player_1.username + " " + props.player_1.turns[0].played_word}</p>
+    }
+      else{
+        gameLog = <p>{props.player_1.username + " played " + props.player_1.turns[0].played_word + " for " + props.player_1.turns[0].points + " points"}</p>
+      }
   }
   
     
